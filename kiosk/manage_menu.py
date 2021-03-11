eb7d8fa22976efffd2a283e3d551f08adb666945
joons5834 <25703159+joons5834@users.noreply.github.com>
2021-03-11 17:55:03 +0900

    manage_menu.py: deleted comments and dead codes

--- kiosk/manage_menu.py
+++ kiosk/manage_menu.py
@@ -10,14 +10,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from kiosk.db import get_db
 
-def set_query(n):
-    if n == 1:
-        return 'SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="햄버거")'
-    elif n == 2:
-        return 'SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="디저트" OR CATEGORY_TAG="치킨")'
-    elif n == 3:
-        return 'SELECT NAME, PRICE FROM MENU WHERE ID IN (SELECT MENU_ID FROM MENU_CATEGORY WHERE CATEGORY_TAG ="음료"  OR CATEGORY_TAG="커피")'
-
 
 bp = Blueprint('manage_menu', __name__, url_prefix='/manage_menu')
 
@@ -38,47 +30,12 @@ def fetch_menu_by_category(menu_cat):
     '''
     db = get_db()
     return db.execute(sql, (menu_cat,)).fetchall()
-# def view_menu():
-#     db = get_db()
-#     c = db.cursor()
-    
-#     global category_tag    
-#     category_tag=1
-#     query = set_query(category_tag)
-#     c.execute(query)
-#     info = c.fetchall()
-#     if request.method == 'GET':
-#         if request.args.get('burger'):
-#             category_tag =1
-#             query = set_query(category_tag)
-#             c.execute(query )
-#             info = c.fetchall()
-#         elif request.args.get('dessert'):
-#             category_tag=2
-#             query = set_query(category_tag)
-#             c.execute(query)
-#             info = c.fetchall()
-#         elif request.args.get('beverage'):
-#             category_tag=3
-#             query = set_query(category_tag)
-#             c.execute(query)
-#             info = c.fetchall()
-#     db.close()
-#     return render_template('/manage_menu/manage_menu.html', data=info)
 
 @bp.route('/detail', methods=['POST'])
 def view_detail():
     db = get_db()
-    #TODO: fetch menu's category
-    
-    
-    # query=set_query(category_tag)
-    # c.execute(query)
-    # info = c.fetchall()
     if request.method=='POST':
         menu_id=request.get_json()
-        # global original_name
-        # original_name = menu_name
         c = db.cursor()
         
         categories = c.execute('''SELECT CATEGORY_TAG FROM MENU_CATEGORY
@@ -107,9 +64,6 @@ def dict_factory(cursor, row):
 def add_menu():
     db = get_db()
     c = db.cursor()
-    # query=set_query(category_tag)
-    # c.execute(query)
-    # info = c.fetchall()
     if request.method == 'POST':
         menu_name = request.form['name']
         menu_image = str(request.form['img'])
@@ -139,23 +93,17 @@ def add_menu():
             c.execute(
                 'INSERT INTO MENU (NAME, IMAGE_PATH, PRICE, DESC, IS_SOLDOUT, WEIGHT_G, KCAL, PROTEIN_G, PROTEIN_PCENT, SODIUM_MG, SODIUM_PCENT, SUGAR_G, SAT_FAT_G, SAT_FAT_PCENT, CAFFEINE_MG, ALLERGY_INFO) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',(menu_name, menu_image, menu_price, menu_desc, 0, menu_weight, menu_kcal, menu_protein_g, menu_protein_pct, menu_sodium_g, menu_sodium_pct, menu_sugar, menu_satfat_g, menu_satfat_pct, menu_caff, menu_allergy)
                 )
-            # c.execute('SELECT ID FROM MENU WHERE ID=?', (menu_id,))
-            # tmp = c.fetchone()
             menu_id = c.lastrowid
             c.execute(
                 'INSERT INTO "MENU_CATEGORY" VALUES (?,?)', (menu_id, menu_category)
                 )
             db.commit()
             db.close()
-            # global name
-            # name = menu_name
-            # return jsonify(done=True, name=menu_name)
             flash(f'{menu_name}을/를 추가하였습니다.')
             return redirect(url_for('manage_menu.view_menu'))
 
         flash(error)
         db.close()
-        # return jsonify(done=False, name=menu_name, error=error)
         return redirect(url_for('manage_menu.view_menu'))
 
 
@@ -164,9 +112,7 @@ def add_menu():
 def change_menu():
     db = get_db()
     c = db.cursor()
-    # query=set_query(category_tag)
-    # c.execute(query)
-    # info = c.fetchall()
+
     if request.method=='POST':
         menu_id = request.form['id']
         menu_name = request.form['name']
@@ -185,19 +131,11 @@ def change_menu():
         menu_caff = float(request.form['caff'])
         menu_allergy = request.form['allergy']
         
-        # c.execute('SELECT ID FROM MENU WHERE NAME=?', (original_name,))
-        # tmp = c.fetchone()
-        # menu_id=tmp[0]
         db.execute('UPDATE MENU SET NAME=?, PRICE=?, DESC=?, WEIGHT_G=?, KCAL=?, PROTEIN_G=?, PROTEIN_PCENT=?, SODIUM_MG=?, SODIUM_PCENT=?, SUGAR_G=?, SAT_FAT_G=?, SAT_FAT_PCENT=?, CAFFEINE_MG=?, ALLERGY_INFO=? WHERE ID=?',(menu_name, menu_price, menu_desc, menu_weight, menu_kcal, menu_protein_g, menu_protein_pct, menu_sodium_g, menu_sodium_pct, menu_sugar, menu_satfat_g, menu_satfat_pct, menu_caff, menu_allergy, menu_id)
                    )
         db.commit()
         db.close()
 
-        # global names
-        # names=[]                                        #변경 전 후 메뉴명 리스트
-        # names.append(original_name)
-        # names.append(menu_name)
-        # return jsonify(done=True)
         flash('메뉴 수정이 완료되었습니다.')
         return redirect(url_for('manage_menu.view_menu'))
 
@@ -205,15 +143,9 @@ def change_menu():
 def delete_menu():
     db = get_db()
     c = db.cursor()
-    # query=set_query(category_tag)
-    # c.execute(query)
-    # info = c.fetchall()
     if request.method=='POST':
         menu_id = request.form['id']
         menu_name=request.form['name']
-        # c.execute('SELECT ID FROM MENU WHERE ID=?', (menu_name,))
-        # tmp = c.fetchone()
-        # menu_id=tmp[0]
         db.execute(
             'DELETE FROM "MENU_CATEGORY" WHERE MENU_ID=?', (menu_id,))
         db.execute(
@@ -221,8 +153,6 @@ def delete_menu():
         db.commit()
         db.close()
 
-        # global name
         name = menu_name
-        # return jsonify(done=True, name=name)
         flash(f'{name}을/를 삭제하였습니다.')
         return redirect(url_for('manage_menu.view_menu'))
